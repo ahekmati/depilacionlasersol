d86f1e12a89471cffc340d7659287cd2f963b004
ahekmati <roninryu7@gmail.com>
2025-01-17 18:04:07 -0500

    update

--- src/components/ui/IntroVedio/IntroVedio.jsx
+++ src/components/ui/IntroVedio/IntroVedio.jsx
@@ -14,7 +14,7 @@ const IntroVedio = () => {
           }}
           streamType="on-demand"
           // The video you will create in Mux, take its playback ID and replace it here.
-          playbackId="S00l2Hhuu6qeVl78Ewqxa8q7Vy00IHkxCADdPxTH02dQ14"
+          playbackId="9o4ePBmJE7REadaaZqo1xo8b6AEyhd7AvQq01Cjl4F01s"
           metadata={{
             video_id: "video-id",
             video_title: "Intro Video",
