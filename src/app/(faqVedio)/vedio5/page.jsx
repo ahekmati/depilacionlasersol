34ac2df7e93a477f2c34e1cf31869fc89c3a9f00
ahekmati <roninryu7@gmail.com>
2025-01-19 13:27:30 -0500

    update

--- src/app/(faqVedio)/vedio5/page.jsx
+++ src/app/(faqVedio)/vedio5/page.jsx
@@ -10,7 +10,7 @@ const page = () => {
         }}
         streamType="on-demand"
         // The video you will create in Mux, take its playback ID and replace it here.
-        playbackId="ylcVjxZTytFnzrFvM2S02skmpPU2W5mCK7tQboBz02tbU"
+        playbackId="nExkBFpflioKuFqkia02uA37WDEkSYWUSYxHLNhtPzDk"
         metadata={{
           video_id: "video-id",
           video_title: "Intro Video",
