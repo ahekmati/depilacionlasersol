825d0305fc9ffabae8a9e49b60998220b6829a4a
ahekmati <roninryu7@gmail.com>
2025-02-04 21:30:09 -0500

    update

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,13 +1,13 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist as GeistSans, Geist_Mono as GeistMono } from "next/font/google";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react";
 
-const geistSans = Geist({
+const geistSans = GeistSans({
   variable: "--font-geist-sans",
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
+const geistMono = GeistMono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
@@ -21,11 +21,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} font-uncut-sans antialiased`}
-   <main className="min-h-screen">{children}</main>
-   <Analytics />
-   <main className="min-h-screen">{children}</main>
+        className={`${geistSans.variable} ${geistMono.variable} font-uncut-sans antialiased`}>
+        <main className="min-h-screen">{children}</main>
+        <Analytics />
       </body>
-  );
-  import { Analytics } from "@vercel/analytics/react"
+    </html>  );
 }
